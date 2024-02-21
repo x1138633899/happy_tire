80a9768f1ffc87fe501d440a1fb5c42b83aec4b4
xingguotao <1138633899@qq.com>
2024-02-21 12:46:47 +0800

    轮胎记录优化

--- src/main/java/com/happiness/happy/tire/server/impl/TireInRecordServiceImpl.java
+++ src/main/java/com/happiness/happy/tire/server/impl/TireInRecordServiceImpl.java
@@ -3,12 +3,16 @@ package com.happiness.happy.tire.server.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.happiness.happy.tire.entity.Response;
 import com.happiness.happy.tire.entity.TireInRecord;
+import com.happiness.happy.tire.entity.TireInfo;
 import com.happiness.happy.tire.mapper.TireInRecordMapper;
+import com.happiness.happy.tire.mapper.TireInfoMapper;
 import com.happiness.happy.tire.server.TireInRecordService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 
 @Service
@@ -17,12 +21,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class TireInRecordServiceImpl extends ServiceImpl<TireInRecordMapper, TireInRecord> implements TireInRecordService {
     @Autowired
     private TireInRecordMapper tireInRecordMapper;
+    @Autowired
+    private TireInfoMapper tireInfoMapper;
 
     @Override
     public Response tireInRecord(TireInRecord tireInRecord) {
         try {
             tireInRecordMapper.insert(tireInRecord);
+            String tireId = tireInRecord.getTireId();
+            synchronized (this) {
+                TireInfo tireInfo = tireInfoMapper.selectById(tireId);
+                if (Objects.nonNull(tireInfo)) {
+                    Integer tireInfoTireNum = tireInfo.getTireNum();
+                    Integer tireNum = tireInRecord.getTireNum();
+                    int realNum = tireNum + tireInfoTireNum;
+                    if (realNum >= 0) {
+                        TireInfo updateData = new TireInfo();
+                        updateData.setTireId(tireId);
+                        updateData.setTireNum(realNum);
+                        int i = tireInfoMapper.updateById(updateData);
+                        log.info("更新轮胎ID<{}>条数成功!更新的数据条数<{}>", tireId, i);
+                    }
+                } else {
+                    log.warn("未找到该轮胎ID<{}>", tireId);
+                    tireInRecord.setRemark("未找到该轮胎ID:" + tireId);
+                    int i = tireInRecordMapper.updateById(tireInRecord);
+                    log.info("更新记录信息,添加备注成功.更新数据条数<{}>", i);
+                }
+            }
             return Response.builder().code(200).msg("添加轮胎进货记录成").object(true).build();
+
         } catch (Exception e) {
             log.error("添加轮胎进货记录失败,失败详情: " + e);
             return Response.builder().code(500).msg("添加轮胎进货记录失败").build();
--- src/main/java/com/happiness/happy/tire/server/impl/TireOutRecordServiceImpl.java
+++ src/main/java/com/happiness/happy/tire/server/impl/TireOutRecordServiceImpl.java
@@ -2,7 +2,9 @@ package com.happiness.happy.tire.server.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.happiness.happy.tire.entity.Response;
+import com.happiness.happy.tire.entity.TireInfo;
 import com.happiness.happy.tire.entity.TireOutRecord;
+import com.happiness.happy.tire.mapper.TireInfoMapper;
 import com.happiness.happy.tire.mapper.TireOutRecordMapper;
 import com.happiness.happy.tire.server.TireOutRecordService;
 import lombok.extern.slf4j.Slf4j;
@@ -10,18 +12,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 
 @Service
 @Slf4j
 @Transactional
 public class TireOutRecordServiceImpl extends ServiceImpl<TireOutRecordMapper, TireOutRecord> implements TireOutRecordService {
     @Autowired
-    private TireOutRecordMapper tireInRecordMapper;
+    private TireOutRecordMapper tireOutRecordMapper;
+    @Autowired
+    private TireInfoMapper tireInfoMapper;
 
     @Override
     public Response tireOutRecord(TireOutRecord tireOutRecord) {
         try {
-            tireInRecordMapper.insert(tireOutRecord);
+            tireOutRecordMapper.insert(tireOutRecord);
+            String tireId = tireOutRecord.getTireId();
+            synchronized (this) {
+                TireInfo tireInfo = tireInfoMapper.selectById(tireId);
+                if (Objects.nonNull(tireInfo)) {
+                    Integer tireInfoTireNum = tireInfo.getTireNum();
+                    Integer tireNum = tireOutRecord.getTireNum();
+                    int realNum = tireNum - tireInfoTireNum;
+                    if (realNum >= 0) {
+                        TireInfo updateData = new TireInfo();
+                        updateData.setTireId(tireId);
+                        updateData.setTireNum(realNum);
+                        int i = tireInfoMapper.updateById(updateData);
+                        log.info("更新轮胎ID<{}>条数成功!更新的数据条数<{}>", tireId, i);
+                    }
+                } else {
+                    log.warn("未找到该轮胎ID<{}>", tireId);
+                    tireOutRecord.setRemark("未找到该轮胎ID:" + tireId);
+                    int i = tireOutRecordMapper.updateById(tireOutRecord);
+                    log.info("更新记录信息,添加备注成功.更新数据条数<{}>", i);
+                }
+            }
             return Response.builder().code(200).msg("添加轮胎销售记录成").object(true).build();
         } catch (Exception e) {
             log.error("添加轮胎进货记录失败,失败详情: " + e);
